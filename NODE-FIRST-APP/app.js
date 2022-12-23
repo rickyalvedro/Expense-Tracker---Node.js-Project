364810eb63cd0232abfba96efdfa4654aeb40c12
Siraj <pintusiraj786@gmail.com>
2022-12-23 12:34:47 +0530

    Understood the request and response

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -1,9 +1,19 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log("Sirajuddin Khan");
+  console.log(req.url, req.method, req.headers);
   // res.end("Hello, I am Siraj");
+  res.setHeader("Content-Type", "test/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  if (req.url === "/home") {
+    res.write("<body><h1>Welcome home</h1></body>");
+  } else if (req.url === "/about") {
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+  } else if (req.url === "/node") {
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+  }
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(4000, "127.0.0.1", () => {
-  console.log("Listening to the requests on port 4000");
-});
+server.listen(8000);
