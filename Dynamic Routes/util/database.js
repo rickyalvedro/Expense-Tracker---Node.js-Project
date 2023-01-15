433de72daa4febbec2fb61bd7fd1d0c7067e7130
Siraj <pintusiraj786@gmail.com>
2023-01-15 16:38:34 +0530

    updated deleted and also got the product

--- Dynamic Routes/util/database.js
+++ Dynamic Routes/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "Pintu#786", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "Pintu#786",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
